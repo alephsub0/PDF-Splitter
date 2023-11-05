64d361849e98056f95d05aa4341362429266a621
Victor <vjose_2007@hotmail.com>
2023-11-04 21:45:39 -0500

    VS - Correcciones: Se agrega una barra de ayuda con una página de créditos y links útiles

---
+++ src/PDF Splitter/Creditos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDF_Splitter
+{
+    public partial class Creditos : Form
+    {
+        public Creditos()
+        {
+            InitializeComponent();
+        }
+
+        private void LLAleph_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            LLAleph.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://go.alephsub0.org/acerca-de");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/alephsub0/PDF-Splitter/blob/main/LICENSE");
+        }
+    }
+}
